b4b4085b497f7353072fd06258ef74c0b336f458
Muaz-Hasan <moaz77hasan@gmail.com>
2024-12-07 12:41:16 +0600

    Implement Express server with MongoDB connection and authentication routes

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+// Database connection
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Database Connected");
+  } catch (err) {
+    console.log("MongoDB Database Connection Failed:", err);
+  }
+};
+
+// Middleware
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// Routes
+app.use("/auth", authRoutes);
+
+app.listen(port, async () => {
+  try {
+    await connect();
+    console.log("Server listening on port", port);
+  } catch (err) {
+    console.error("Error starting server:", err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
